03887c6a93a650c9f31d46474bae6683c98f6d89
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-11-03 14:51:42 -0700

    implement auth middleware to populate m query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,26 +4,39 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        },
+
         // get all users
         users: async () => {
             return User.find()
-            .select('-__v -password')
-            .populate('thoughts')
-            .populate('friends');
+                .select('-__v -password')
+                .populate('thoughts')
+                .populate('friends');
         },
         // get a user by username
         user: async (parent, { username }) => {
             return User.findOne({ username })
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
-            return Thought.find(params).sort({ createdAt: -1});
+            return Thought.find(params).sort({ createdAt: -1 });
         },
 
-        thought: async (parent, { _id}) => {
+        thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
         }
     },
@@ -34,7 +47,7 @@ const resolvers = {
 
             return { token, user };
         },
-        login: async (parent, {email, password}) => {
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
             if (!user) {
@@ -46,7 +59,7 @@ const resolvers = {
             if (!correctPw) {
                 throw new AuthenticationError('Incorrect credentials');
             }
-            
+
             const token = signToken(user);
             return { token, user };
         }
