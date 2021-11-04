d2eb8340ffb01f6cce9f03e0b75e0dca5658a0c2
eisforgene <82473195+eisforgene@users.noreply.github.com>
2021-11-04 01:32:02 -0700

    fixed bug that didn't display thoughts on page

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -8,22 +8,22 @@ const ThoughtList = ({ thoughts, title }) => {
     return (
         <div>
             <h3>{title}</h3>
-        {thoughts &&
-        thoughts.map(thought => (
-            <div key={thought._id} className="card mb-3">
-                <p className="card-header">
-                    {thought.username}
-                    thought on {thought.createdAt}
-                </p>
-                <div className="card-body">
-                    <p>{thought.thoughtText}</p>
-                    <p className="mb-0">
-                        Reactions: {thought.reactionCount} || Click to{' '}
-                        {thought.reactionCount ? 'see' : 'start'} the discussion!
-                    </p>
-                </div>
-            </div>
-        ))}
+            {thoughts &&
+                thoughts.map(thought => (
+                    <div key={thought._id} className="card mb-3">
+                        <p className="card-header">
+                            {thought.username}
+                            thought on {thought.createdAt}
+                        </p>
+                        <div className="card-body">
+                            <p>{thought.thoughtText}</p>
+                            <p className="mb-0">
+                                Reactions: {thought.reactionCount} || Click to{' '}
+                                {thought.reactionCount ? 'see' : 'start'} the discussion!
+                            </p>
+                        </div>
+                    </div>
+                ))}
         </div>
     );
 };
